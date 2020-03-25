548a84202d0cbd12e81433ee012d0e8097ebab5f
zqmillet <zqmillet@qq.com>
2020-03-25 23:01:50 +0800

    [Project] 添加 .gitignore 文件

---
+++ tester/utilities/testcase/base_testcase.py
@@ -0,0 +1,9 @@
+
+class BaseTestCase(metaclass = ):
+    """
+    description: BaseTestCase 是测试用例的基类, 所有的测试用例都需要继承 BaseTestCase.
+    """
+
+    @classmethod
+    def __initialize__(cls):
+        pass
---
+++ tester/utilities/testcase/meta_class.py
@@ -0,0 +1,6 @@
+class MetaClass(type):
+    """
+    description: 这个是测试用例基类 BaseTestCase 的元类.
+    """
+    def __prepare__(self):
+        pass
