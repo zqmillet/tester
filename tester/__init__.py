4faad4c032eb54cdbb07be24144f2f710cdaa9c9
zqmillet <zqmillet@qq.com>
2020-03-25 23:12:35 +0800

    [Test] 添加新的测试用例

---
+++ tester/__init__.py
@@ -0,0 +1,1 @@
+from tester.utilities.testcase import BaseTestCase
---
+++ tester/testcases/demonstration/Demonstration.py
@@ -0,0 +1,10 @@
+from tester import BaseTestCase
+
+class Demonstration(BaseTestCase):
+    """
+    description: 这个用例是 tester 测试框架的示例用例.
+    author: zqmillet
+    preconditions:
+        - 无
+        
+    """
